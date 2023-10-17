4391a41669310c6a8e5f90b91a2bb1bb91a5bc6d
WENHUAN22 <zengwenhuan@gmail.com>
2023-10-17 15:56:44 +0000

    add

--- scripts/models.py
+++ scripts/models.py
@@ -8,6 +8,7 @@ from torch.nn import CrossEntropyLoss
 import torch
 from torchcrf import CRF
 from methyl_loss import MethyLoss
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, roc_auc_score, matthews_corrcoef, confusion_matrix
 
 
 def compute_metrics(labels, preds):
